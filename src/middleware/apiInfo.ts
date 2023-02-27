50fe9c74c672a80d9e63c6fef8699d444d2cfb60
shuli495 <xiaoyao495@163.com>
2023-02-27 16:43:38 +0800

    update: 统计中间件支持环境变量

--- src/middleware/apiInfo.ts
+++ src/middleware/apiInfo.ts
@@ -3,9 +3,18 @@ import { Context } from 'koa';
 /**
  * 接口统计
  */
-export default async function apiInfo(ctx: Context, next: any) {
-    const beginTime = new Date().getTime();
-    await next();
-    const endTime = new Date().getTime();
-    console.log(`时间：${endTime - beginTime} 接口：${ctx.url}`);
-}
+export default (enableEnv: Array<string> = []) => {
+    const apiInfo = async (ctx: Context, next: any) => {
+        if (enableEnv.length > 0 && !enableEnv.concat(process.env.NODE_ENV)) {
+            await next();
+            return;
+        }
+
+        const beginTime = new Date().getTime();
+        await next();
+        const endTime = new Date().getTime();
+        console.log(`时间：${endTime - beginTime} 接口：${ctx.url}`);
+    };
+
+    return apiInfo;
+};
